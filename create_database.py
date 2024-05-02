c9177a3a7f6a400a3b76a4ba2f6316530f9ca4d5
anderTron1 <andre-luizpiresguimaraes@outlook.com>
2024-05-01 22:07:19 -0300

    ajuste no create_database.py para criar pastas importantes

--- create_database.py
+++ create_database.py
@@ -4,7 +4,6 @@ Created on Sun Apr 28 11:21:23 2024
 
 @author: user
 """
-
 import sqlite3
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Table, create_engine
@@ -21,8 +20,19 @@ for i in range(len(sys.argv)):
         break
 
 diretory = 'conteudos'
-conn = sqlite3.connect(f'databases\\data_{name_path}.sqlite')
-engine = create_engine(f'sqlite:///databases\\data_{name_path}.sqlite')
+diretory_support = 'apoio'
+diretory_df = 'databases'
+
+#Verificar se algumas pastas importantes existe, caso não cria elas
+if not os.path.exists(f'{diretory}/{diretory_support}'):
+    os.makedirs(f'{diretory}/{diretory_support}')
+    
+if not os.path.exists(diretory_df):
+    os.makedirs(diretory_df)
+
+#Carregando elementos do sqlite
+conn = sqlite3.connect(f'{diretory_df}\\data_{name_path}.sqlite')
+engine = create_engine(f'sqlite:///{diretory_df}\\data_{name_path}.sqlite')
 db = SQLAlchemy()
 
 class Users(db.Model, UserMixin):
@@ -35,7 +45,7 @@ Users_table = Table('users', Users.metadata)
 def create_users_table():
     Users.metadata.create_all(engine)
 
-if not os.path.exists("databases\\data_{name_path}.sqlite"):
+if not os.path.exists("{diretory_df}\\data_{name_path}.sqlite"):
     create_users_table()
 
 if os.path.exists(diretory):
@@ -44,6 +54,7 @@ if os.path.exists(diretory):
 Session = sessionmaker(bind=engine)
 session = Session()
 
+#Criar um usuario e senha padrão de administrador 
 hashed_password = generate_password_hash('123', method='pbkdf2:sha256')
 ins = Users_table.insert().values(username='admin', password=hashed_password)
 conn = engine.connect()
